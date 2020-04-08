796340b5e24b684c876142c7a6d0ab6a1241f40b
Dawid Kozakiewicz <turgor1996@gmail.com>
2020-04-08 23:53:09 +0200

    route files with express router

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ const express = require("express");
 const connectDB = require("./config/db");
 
 const app = express();
+const doo = require("./routes/api/auth");
 
+console.log(doo);
 //Connect Database
 connectDB();
 
 app.get("/", (req, res) => res.send("API running"));
+
+// Define Routes
+app.use("api/users", require("./routes/api/users"));
+app.use("api/auth", require("./routes/api/auth"));
+app.use("api/profile", require("./routes/api/profile"));
+app.use("api/posts", require("./routes/api/posts"));
 
 const PORT = process.env.PORT || 5000;
 
